ed255c6379b39f2dfb82246b3a78499d169efd62
MA Laforge <ma.laforge.49@gmail.com>
2016-12-13 23:39:45 -0500

    Improve API to manipulate Plot2D instead of Multiplot.
    Also use NumericIO for annotation.

--- notebook/BodePlots.jl
+++ notebook/BodePlots.jl
@@ -1,6 +1,8 @@
 module BodePlots
 
 using InspectDR
+import InspectDR: Plot2D
+using NumericIO
 using Colors
 
 #Colour constants
@@ -11,6 +13,7 @@ const red = RGB24(1, 0, 0); const green = RGB24(0, 1, 0); const blue = RGB24(0, 
 #Convenience aliases
 #-------------------------------------------------------------------------------
 mag(x) = abs(x); phase(x) = rad2deg(angle(x))
+SI(x) = formatted(x, :SI, ndigits=3)
 const j = im
 
 
@@ -44,6 +47,27 @@ f_3dB(f, G, f_z, f_p1, f_p2) =
 #==Main functions
 ===============================================================================#
 
+#Initialize Bode subplots:
+#-------------------------------------------------------------------------------
+function init(plot_mag::Plot2D, plot_phase::Plot2D)
+	plot_mag.axes = InspectDR.axes(:log10, :dB20)
+	a = plot_mag.annotation; lyt = plot_mag.layout
+	a.title = ""; lyt.htitle = 8 #No subtitle/space
+	a.xlabel = ""; lyt.haxlabel = 8 #No axis label/space
+	a.ylabel = "Magnitude (dB)"
+
+	plot_phase.axes = InspectDR.axes(:log10, :lin)
+	a = plot_phase.annotation; lyt = plot_phase.layout
+	a.title = ""; lyt.htitle = 8 #subtitle/space
+	a.xlabel = "Frequency (Hz)"; a.ylabel = "Phase (°)"
+
+	for splot in [plot_mag, plot_phase]
+		splot.layout.grid = grid(vmajor=true, vminor=true, hmajor=false)
+	end
+	
+	return
+end
+
 #Create new Bode plot:
 #-------------------------------------------------------------------------------
 function new()
@@ -55,61 +79,46 @@ function new()
 	mplot.wplot = w; mplot.hplot = h/2
 
 	plot_mag = add(mplot, InspectDR.Plot2D)
-		plot_mag.axes = InspectDR.axes(:log10, :dB20)
-		a = plot_mag.annotation; lyt = plot_mag.layout
-		a.title = ""; lyt.htitle = 8 #No subtitle/space
-		a.xlabel = ""; lyt.haxlabel = 8 #No axis label/space
-		a.ylabel = "Magnitude (dB)"
-
 	plot_phase = add(mplot, InspectDR.Plot2D)
-		plot_phase.axes = InspectDR.axes(:log10, :lin)
-		a = plot_phase.annotation; lyt = plot_phase.layout
-		a.title = ""; lyt.htitle = 8 #subtitle/space
-		a.xlabel = "Frequency (Hz)"; a.ylabel = "Phase (°)"
 
-	for splot in [plot_mag, plot_phase]
-		splot.layout.grid = grid(vmajor=true, vminor=true, hmajor=false)
-	end
-	
+	init(plot_mag, plot_phase)
+
 	return mplot
 end
 
 #Modify previously-generated Bode plot (base algorithm):
 #-------------------------------------------------------------------------------
-function update(mplot::InspectDR.Multiplot, f::Vector, y::Vector)
+function update(plot_mag::Plot2D, plot_phase::Plot2D, f::Vector, y::Vector)
 	const ldata = line(color=blue, width=3, style=:solid)
 
-	plot_mag = mplot.subplots[1]
-		plot_mag.data = [] #Clear old data
-		plot_mag.ext_full = InspectDR.PExtents2D() #Reset full extents
-		wfrm = add(plot_mag, f, mag(y))
-		wfrm.line = ldata
-		plot_mag.ext = InspectDR.PExtents2D(NaN, NaN, -10, 25)
+	plot_mag.data = [] #Clear old data
+	plot_mag.ext_full = InspectDR.PExtents2D() #Reset full extents
+	wfrm = add(plot_mag, f, mag(y))
+	wfrm.line = ldata
+	plot_mag.ext = InspectDR.PExtents2D(NaN, NaN, -10, 25)
 
-	plot_phase = mplot.subplots[2]
-		plot_phase.data = [] #Clear old data
-		plot_phase.ext_full = InspectDR.PExtents2D() #Reset full extents
-		wfrm = add(plot_phase, f, phase(y))
-		wfrm.line = ldata
-		plot_phase.ext = InspectDR.PExtents2D(NaN, NaN, -180, 0)
+	plot_phase.data = [] #Clear old data
+	plot_phase.ext_full = InspectDR.PExtents2D() #Reset full extents
+	wfrm = add(plot_phase, f, phase(y))
+	wfrm.line = ldata
+	plot_phase.ext = InspectDR.PExtents2D(NaN, NaN, -180, 0)
 
-	return mplot
+	return
 end
 
 #Update annotation on Bode plot (enabled=false to clear annotation):
 #-------------------------------------------------------------------------------
-function update_annotation(mplot::InspectDR.Multiplot, f0, fBW, phase0, enabled=true)
+function update_annotation(plot_mag::Plot2D, plot_phase::Plot2D, f0, fBW, phase0, enabled=true)
 	const lmarker = line(style=:dash, width=2.5)
 	const lmarker_light = line(style=:dash, width=2.5, color=RGB24(.4,.4,.4))
 
-	plot_mag = mplot.subplots[1]
-		plot_mag.markers = [] #Clear old markers
-		plot_mag.atext = [] #Clear old annotation
-	plot_phase = mplot.subplots[2]
-		plot_phase.markers = [] #Clear old markers
-		plot_phase.atext = [] #Clear old annotation
+	plot_mag.markers = [] #Clear old markers
+	plot_mag.atext = [] #Clear old annotation
 
-	if !enabled; return mplot end
+	plot_phase.markers = [] #Clear old markers
+	plot_phase.atext = [] #Clear old annotation
+
+	if !enabled; return; end
 
 	#Add specialized annotation
 	afont = InspectDR.Font(12) #Font for annotation
@@ -125,13 +134,13 @@ function update_annotation(mplot::InspectDR.Multiplot, f0, fBW, phase0, enabled=
 	add(plot_mag, hmarker(1, lmarker_light))
 
 	if isfinite(fBW)
-		fstr = "f3dB=" * @sprintf("%.1f MHz", fBW/1e6)
+		fstr = "f3dB=" * SI(fBW) * "Hz"
 		add(plot_mag, atext(fstr, x=fBW, xoffset=-1/100, yoffset=.5, font=afont, angle=-90, align=:bc))
 	end
 
 	#Hack if f0>0: TODO: Fix bug where annotating @ 0 on log plot causes glitch. 
 	if isfinite(f0) && f0 > 0
-		fstr = "f0=" * @sprintf("%.1f MHz", f0/1e6)
+		fstr = "f0=" * SI(f0) * "Hz"
 		add(plot_mag, atext(fstr, x=f0, xoffset=-1/100, yoffset=.5, font=afont, angle=-90, align=:bc))
 	end
 
@@ -143,25 +152,35 @@ function update_annotation(mplot::InspectDR.Multiplot, f0, fBW, phase0, enabled=
 		add(plot_phase, hmarker(phase0, lmarker))
 	end
 
-	return mplot
+	return
+end
+
+#Modify previously-generated Bode plot:
+#-------------------------------------------------------------------------------
+#(fmin, fmax, f_p1: in Hz)
+function update(plot_mag::Plot2D, plot_phase::Plot2D, fmin, fmax, G, f_z, f_p1, f_p2, annot=true)
+	const npts = 100
+
+	f = logspace(log10(fmin), log10(fmax), npts)
+	y = xf_o2(f, G, f_z, f_p1, f_p2)
+	update(plot_mag, plot_phase, f, y)
+
+	f0 = f_0(f, G, f_z, f_p1, f_p2) #Unity gain frequency
+	fBW = f_3dB(f, G, f_z, f_p1, f_p2)
+	phase0 = phase(xf_o2(f0, G, f_z, f_p1, f_p2)) #Unity gain phase
+	update_annotation(plot_mag, plot_phase, f0, fBW, phase0, annot)
+
+	return
 end
 
 #Modify previously-generated Bode plot:
 #-------------------------------------------------------------------------------
 #(fmin, fmax, f_p1: in Hz)
 function update(mplot::InspectDR.Multiplot, fmin, fmax, GdB, f_z, f_p1, f_p2, annot=true)
-	const npts = 100
+	const plot_mag = mplot.subplots[1]
+	const plot_phase = mplot.subplots[2]
 	const G = 10.0^(GdB/20)
-
-	f = logspace(log10(fmin), log10(fmax), npts)
-	y = xf_o2(f, G, f_z, f_p1, f_p2)
-	update(mplot, f, y)
-
-	f0 = f_0(f, G, f_z, f_p1, f_p2) #Unity gain frequency
-	fBW = f_3dB(f, G, f_z, f_p1, f_p2)
-	phase0 = phase(xf_o2(f0, G, f_z, f_p1, f_p2)) #Unity gain phase
-	update_annotation(mplot, f0, fBW, phase0, annot)
-
+	update(plot_mag, plot_phase, fmin, fmax, G, f_z, f_p1, f_p2, annot)
 	return mplot
 end
 
