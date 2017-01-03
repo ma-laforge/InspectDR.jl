439b9bc68eea67920398a3d0049923524c3e6c6b
MA Laforge <ma.laforge.49@gmail.com>
2017-01-03 18:14:08 -0500

    Add new glyph test.

---
+++ sample/demo8.jl
@@ -0,0 +1,49 @@
+#Demo 8: Glyph test
+#-------------------------------------------------------------------------------
+using InspectDR
+using Colors
+import Graphics: width, height
+
+
+#==Input
+===============================================================================#
+
+#Constants
+#-------------------------------------------------------------------------------
+black = RGB24(0, 0, 0)
+white = RGB24(1, 1, 1)
+red = RGB24(1, 0, 0)
+green = RGB24(0, 1, 0)
+blue = RGB24(0, 0, 1)
+
+
+#Input data
+#-------------------------------------------------------------------------------
+x = collect(1:10)
+y0 = x.*0
+_glyphs = InspectDR.SUPPORTED_GLYPHS
+#_colors = Colors.colormap("Oranges", length(_glyphs))
+_colors = Colors.distinguishable_colors(length(_glyphs))
+
+#==Generate plot
+===============================================================================#
+mplot = InspectDR.Multiplot(title="Glyph Tests")
+mplot.ncolumns = 1
+
+plot = add(mplot, InspectDR.Plot2D())
+	plot.ext_full = InspectDR.PExtents2D(xmin=0,xmax=11,ymin=0,ymax=length(_glyphs)+1)
+	plot.layout.legend.enabled=true
+
+	a = plot.annotation
+#	a.title = "Glyph Test"
+
+	for (i, g) in enumerate(_glyphs)
+		wfrm = add(plot, x, y0+(length(_glyphs)+1-i), id="$g")
+		wfrm.line = line(style=:solid, color=_colors[i], width=2)
+		wfrm.glyph = glyph(shape=g, size=10, color=black, fillcolor=_colors[i])
+	end
+
+
+gplot = display(InspectDR.GtkDisplay(), mplot)
+
+:DONE
